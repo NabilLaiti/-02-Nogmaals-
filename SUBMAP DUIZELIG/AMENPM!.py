ff200c3fa973a690dc380642362e776ceeb5dcf0
Straatmongol <99067444@mydavinci.nl>
2021-09-30 09:22:43 +0200

    AM EN PM!

---
+++ SUBMAP DUIZELIG/AMENPM!.py
@@ -0,0 +1,17 @@
+import time
+
+i = 12
+while i >= 1:
+    print(i,'AM')
+    time.sleep(1)
+
+    i = i - 1
+
+j = 12
+while j >= 1:
+    print(j,'PM')
+    time.sleep(1)
+    j = j - 1
+
+
+#Nagekekn milan!
